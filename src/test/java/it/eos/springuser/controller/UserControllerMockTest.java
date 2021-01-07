552eb3b1e64ebbdcd3c6dd865a78ad8caf098c7c
Daniela De Pascali <danidep@outlook.it>
2021-01-07 15:37:40 +0100

    new test

--- src/test/java/it/eos/springuser/controller/UserControllerMockTest.java
+++ src/test/java/it/eos/springuser/controller/UserControllerMockTest.java
@@ -2,38 +2,33 @@ package it.eos.springuser.controller;
 
 import it.eos.springuser.model.UserModel;
 import it.eos.springuser.repository.UserEntity;
-import it.eos.springuser.repository.UserRepository;
+import it.eos.springuser.service.UserService;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.json.JacksonTester;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpServletResponse;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
-@ExtendWith(SpringExtension.class)
 @AutoConfigureJsonTesters
-@SpringBootTest
-@AutoConfigureMockMvc
+@WebMvcTest(UserController.class)
 public class UserControllerMockTest {
 
     @Autowired
     private MockMvc mvc;
 
     @MockBean
-    private UserRepository userRepository;
+    private UserService userService;
 
     @Autowired
     private JacksonTester<UserModel> userEntityJacksonTester;
@@ -54,7 +49,7 @@ public class UserControllerMockTest {
         userModel.setPassword("333");
         userModel.setActive(false);
 
-        given(userRepository.findById(1L)).willReturn(java.util.Optional.of(userEntity));
+        Mockito.when(userService.getUserById(any(long.class))).thenReturn(userModel);
 
         //when
         MockHttpServletResponse response = mvc.perform(get("/user/1")
@@ -62,10 +57,66 @@ public class UserControllerMockTest {
                 .andReturn().getResponse();
 
         //then
-        assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
-        assertThat(response.getContentAsString()).isEqualTo(
-                userEntityJacksonTester.write(userModel).getJson());
+        Assertions.assertEquals(HttpStatus.OK.value(), response.getStatus());
+        Assertions.assertEquals(response.getContentAsString(), userEntityJacksonTester.write(userModel).getJson());
 
+    }
+
+    @Test
+    void canSaveTheUser() throws Exception {
+
+        //given
+        UserEntity userEntity = new UserEntity();
+        userEntity.setMail("prova@mail.com");
+        userEntity.setName("test");
+        userEntity.setPassword("333");
+        userEntity.setActive(false);
+
+        UserModel userModel = new UserModel();
+        userModel.setMail("prova@mail.com");
+        userModel.setName("test");
+        userModel.setPassword("333");
+        userModel.setActive(false);
+
+        Mockito.when(userService.save(any(UserModel.class))).thenReturn(userModel);
+
+        //when
+        MockHttpServletResponse response = mvc.perform(post("/user")
+                .accept(MediaType.APPLICATION_JSON))
+                .andReturn().getResponse();
+
+        //then
+        Assertions.assertEquals(HttpStatus.OK.value(), response.getStatus());
+        Assertions.assertEquals(response.getContentAsString(), userEntityJacksonTester.write(userModel).getJson());
+
+    }
+
+    @Test
+    void canUpdateTheUser() throws Exception {
+
+        //given
+        UserEntity userEntity = new UserEntity();
+        userEntity.setMail("prova@mail.com");
+        userEntity.setName("test");
+        userEntity.setPassword("333");
+        userEntity.setActive(false);
+
+        UserModel userModel = new UserModel();
+        userModel.setMail("prova@mail.com");
+        userModel.setName("test");
+        userModel.setPassword("333");
+        userModel.setActive(false);
+
+        Mockito.when(userService.putUser(any(UserModel.class))).thenReturn(userModel);
+
+        //when
+        MockHttpServletResponse response = mvc.perform(put("/user")
+                .accept(MediaType.APPLICATION_JSON))
+                .andReturn().getResponse();
+
+        //then
+        Assertions.assertEquals(HttpStatus.OK.value(), response.getStatus());
+        Assertions.assertEquals(response.getContentAsString(), userEntityJacksonTester.write(userModel).getJson());
     }
 
 }
